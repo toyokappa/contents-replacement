e427fea608f1f7cf1dd8a52c9b022263eff0dcdc
Yuta Toyokawa <kppg42@gmail.com>
2017-11-03 19:57:03 +0900

    Setup themes_controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,10 +1,30 @@
 class ThemesController < ApplicationController
   def index
+    @themes = Theme.all
+  end
+
+  def show
+    @theme = Theme.find(params[:id])
+    @replacements = @theme.replacements.order(:order)
   end
 
   def edit
+    @theme = Theme.find(params[:id])
+    @theme.replacements.build
   end
 
   def update
+    @theme = Theme.find(params[:id])
+    if @theme.update(theme_params)
+      redirect_to @theme
+    else
+      render "edit"
+    end
   end
+
+  private
+
+    def theme_params
+      params.require(:theme).permit(:name, replacements_attributes: [:id, :name, :order, :_destroy])
+    end
 end
