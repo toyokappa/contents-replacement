fa996d24d110ef729090eb62c26ab28259f770ad
Yuta Toyokawa <kppg42@gmail.com>
2017-11-08 19:51:59 +0900

    Change order logic

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,11 @@
 class Theme < ApplicationRecord
+  TOP_CONTENTS = {
+    content1: "コンテンツ1",
+    content2: "コンテンツ2",
+    content3: "コンテンツ3"
+  }
+  TOP_CONTENTS_DEFAULT_ORDER = TOP_CONTENTS.keys
+
+  serialize :contents_order, Hash
   validates :name, presence: true
 end
